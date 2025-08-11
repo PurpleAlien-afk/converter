ddd139f5d63c1210813e8a5f8fcb7cafee689975
PurpleAlien-afk <iamthinking296@gmail.com>
2025-08-12 02:13:43 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('romaji2jp-cache').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'manifest.json',
-                'service-worker.js',
-                'https://unpkg.com/wanakana'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
